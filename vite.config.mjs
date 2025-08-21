eb3bdcbbcdda40167e91bce30e97c8848f00912a
Cherryszyx <138777483+Cherryszyx@users.noreply.github.com>
2025-08-21 08:24:10 +0700

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-     build: {
-    manifest: true,
-    outDir: 'public/build',
-         
     server: {
         host: true, // Wajib untuk akses dari HP (LAN)
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
     },
     plugins: [
         laravel({
